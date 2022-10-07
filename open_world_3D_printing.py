b2f5ea69c4815233cb4040d7d2ea681c93374dd3
Andrew Wasielewski <awwasielewski@gmail.com>
2022-10-06 17:08:25 -0700

    adding 3d file

---
+++ open_world_3D_printing.py
@@ -0,0 +1,3 @@
+"""
+Create a manuvrable 3D world in which a 3d printer on wheels can move around and print. Testing/Practice for frontier robotic 3d printer. This is so 3d printer is not bounded by its rail-based movement system
+"""
