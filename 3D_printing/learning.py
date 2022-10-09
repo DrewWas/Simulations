91eb87c39bbf2cfd438b31c3c7f30cd13ba56c80
Andrew Wasielewski <awwasielewski@gmail.com>
2022-10-09 14:03:44 -0700

    I am trying to learn

--- 3D_printing/learning.py
+++ 3D_printing/learning.py
@@ -1,12 +1,30 @@
+import os
 from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
 from panda3d.core import *
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
         properties = WindowProperties()
         properties.setSize(1000,700)
         self.win.requestProperties(properties) 
+
+
+
+        self.environment = loader.loadModel("Models/environment")
+        self.environment.reparentTo(render)
+			
+        self.tempActor = Actor("Models/panda-model", {"walk" : "Models/panda-model_run"})
+        self.tempActor.reparentTo(render)
+        self.tempActor.setPos(0,7,0)
+        self.tempActor.setScale(0.1)
+        self.tempActor.loop("walk")
+
+
+
+
 
 game = Game()
 game.run()
