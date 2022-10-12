cf1e2f40cb9cecdf9e580a1034ea84b4f7536cfa
Andrew Wasielewski <awwasielewski@gmail.com>
2022-10-12 13:09:11 -0700

    I can draw a grid

--- SceneGen/threeD_Terrain.py
+++ SceneGen/threeD_Terrain.py
@@ -3,13 +3,15 @@ import pygame
 pygame.init()
 pygame.display.init()
 
-window = pygame.display.set_mode((1000,700))
+window = pygame.display.set_mode((900,900))
 pygame.display.set_caption("terrain")
 
 def draw():
-    pygame.draw.rect(window, (0,138,255), pygame.Rect(250,100,500,500), 1)
-    for i in range(100):
-        pygame.draw.polygon(window, (0,138,255), ((250,100), (270,100), (260, 120)), 1)
+    x = 100
+    y = 100
+    for i in range(40):
+        for j in range(40):
+            pygame.draw.rect(window, (0,138,255), pygame.Rect(50 + (20 * i),50 + (20 * j),20,20), 1)
 
 def main():
     run = True
